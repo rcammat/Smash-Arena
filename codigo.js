f4310a7789ba837d468a4ff52f4cd56fd3b5c83f
Franciscojmm <61140966+Franciscojmm@users.noreply.github.com>
2022-01-03 19:26:03 +0100

    Create codigo.js

---
+++ codigo.js
@@ -0,0 +1,8 @@
+var oGestion =  new Gestion();
+
+alert(oGestion.altaUsuario(new Usuario("Pepe","123456678e","prueba")));
+alert(oGestion.altaUsuario(new Usuario("Pepe","123456678e","prueba")));
+oGestion.aEdificios.push(new Edificio("Nuevo Edificio",01,12345));// Creamos un edifico.
+oGestion.aEdificios[0].pistas.push(new Pista("Pista 1",01,01));//En ese edificio creamos esta pista
+
+console.log(oGestion.altaReserva(new Reserva("PepeReserva","Vamoh a jugar",new Date(01/02/2022),17,01,1)));//En esa pista creamos una reserva.
