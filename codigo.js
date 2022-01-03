f980fba2f9151ecdbc4b24db3594c3cc164c4853
Raimundo Campos Mateos <71877785+rcammat@users.noreply.github.com>
2022-01-03 19:36:49 +0100

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -2,6 +2,8 @@ var oGestion =  new Gestion();
 
 alert(oGestion.altaUsuario(new Usuario("Pepe","123456678e","prueba")));
 alert(oGestion.altaUsuario(new Usuario("Pepe","123456678e","prueba")));
+console.log(oGestion.altaUsuario(new Usuario("Carlos","123456789J","prueba")));
+console.log(oGestion.modificarUsuario("123456789J","Juan","prueba2","Pendiente"));
 oGestion.aEdificios.push(new Edificio("Nuevo Edificio",01,12345));// Creamos un edifico.
 oGestion.aEdificios[0].pistas.push(new Pista("Pista 1",01,01));//En ese edificio creamos esta pista
 
