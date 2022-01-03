57ad74933b286233a858e080f73294bb08cfce4e
Raimundo Campos Mateos <71877785+rcammat@users.noreply.github.com>
2022-01-03 19:35:56 +0100

    Add files via upload

--- objeto.js
+++ objeto.js
@@ -12,6 +12,20 @@ class Gestion {
             return false;
         }
     }
+    modificarUsuario(sDNI,sNombreApe,sFoto,sEstado){
+
+        let aUsuario = oGestion.aUsuarios.filter(oUsu => oUsu.DNI == sDNI);
+        aUsuario[0].DNI = sDNI;
+        aUsuario[0].NombreAp = sNombreApe;
+        aUsuario[0].Foto = sFoto;
+        aUsuario[0].Estado = sEstado;
+        return oGestion.aUsuarios;
+    }
+    altaReserva(oReserva)
+    {
+        oGestion.aEdificios[0].pistas[0].reservas.push(oReserva);
+        return oGestion.aEdificios[0].pistas[0].reservas;
+    }
 }
 class Edificio {
     constructor (sNombre,iId,iCp)
@@ -21,6 +35,9 @@ class Edificio {
       this.iCp = iCp;
       this.aIdPistas = [];
     }
+    get pistas(){
+        return this.aIdPistas;
+    }
   }
 class Pista{
     constructor (sNombre,iNumPista,iIdEdificio)
@@ -28,20 +45,27 @@ class Pista{
       this.sNombre = sNombre;
       this.iNumPista = iNumPista; 
       this.iIdEdificio = iIdEdificio;
+      this.aReservas = [];
+    }
+    get reservas(){
+        return this.aReservas;
     }
 }
 
 class Reserva{
-    constructor (sNombreReserva,sDescripcion,dDiaReserva,iHoraInicio,iIdPista,aIdsUsuarios,bOcupada,iOcupacion)
+    constructor (sNombreReserva,sDescripcion,dDiaReserva,iHoraInicio,iIdPista,iOcupacion)
     {
       this.sNombreReserva = sNombreReserva;
       this.sDescripcion = sDescripcion; 
       this.dDiaReserva = dDiaReserva;
       this.iHoraInicio = iHoraInicio;
       this.iIdPista = iIdPista;
-      this.aIdsUsuarios = aIdsUsuarios;
-      this.bOcupada = bOcupada;
+      this.aIdsUsuarios = [];
+      this.bOcupada = false;
       this.iOcupacion=iOcupacion;
+    }
+    get pista(){
+        return this.iIdPista;
     }
 }
 class Usuario {
@@ -53,9 +77,28 @@ class Usuario {
         this.bEsSocio = false;
         this.aClases = [];
     }
+    get NombreAp(){
+        return this.sNombreAp;
+    }
+    set NombreAp(sNombreAp){
+        this.sNombreAp = sNombreAp;
+    }
     get DNI(){
         return this.sDNI;
     }
+    set DNI(sDNI){
+        this.sDNI = sDNI;
+    }
+    set Foto(sFoto){
+        this.sFoto = sFoto;
+    }
+    set Estado(sEstado){
+        this.sEstado = sEstado;
+    }
+    set EsSocio(bEsSocio){
+        this.bEsSocio = bEsSocio;
+    }
+
 }
 
 class Clase {
